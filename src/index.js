25491b18569711a9589b57deec961c852d0b2cdc
Shen <youchuangqiba@163.com>
2018-05-09 20:31:58 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export { default as milk } from './milk';
-export { default as grow } from './grow';
-export { default as $ } from './wrap';
-export { default as diff } from './diff';
+export { default as milk } from './milk'
+export { default as grow } from './grow'
+export { default as $ } from './wrap'
+export { default as diff } from './diff'
